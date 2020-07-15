933537c11264b5290f7594408e668867244d3ed3
Michael Aerni <hello@michaelaerni.ch>
2020-07-15 14:34:33 +0200

    Add tests boilerplate and github workflow

---
+++ tests/TestCase.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Aerni\Snipcart\Tests;
+
+use Aerni\Snipcart\ServiceProvider;
+use Illuminate\Foundation\Testing\WithFaker;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Statamic\Extend\Manifest;
+use Statamic\Providers\StatamicServiceProvider;
+use Statamic\Statamic;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    use WithFaker;
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            StatamicServiceProvider::class,
+            ServiceProvider::class,
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Statamic' => Statamic::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app->make(Manifest::class)->manifest = [
+            'aerni/statamic-snipcart' => [
+                'id' => 'aerni/snipcart',
+                'namespace' => 'Aerni\\Snipcart\\',
+            ],
+        ];
+
+        Statamic::pushActionRoutes(function () {
+            return require_once realpath(__DIR__.'/../routes/actions.php');
+        });
+    }
+
+    protected function resolveApplicationConfiguration($app)
+    {
+        parent::resolveApplicationConfiguration($app);
+
+        $configs = [
+            'assets', 'cp', 'forms', 'static_caching',
+            'sites', 'stache', 'system', 'users',
+        ];
+
+        foreach ($configs as $config) {
+            $app['config']->set("statamic.$config", require(__DIR__."/../vendor/statamic/cms/config/{$config}.php"));
+        }
+
+        $app['config']->set('statamic.users.repository', 'file');
+        $app['config']->set('statamic.stache', require(__DIR__.'/__fixtures__/config/statamic/stache.php'));
+    }
+
+}
