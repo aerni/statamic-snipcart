3a7ad0bcf3b7fc3655fd532df750454c0d0ed352
Michael Aerni <hello@michaelaerni.ch>
2020-07-20 14:58:02 +0200

    Move attributes to Validator class

--- src/Validator.php
+++ src/Validator.php
@@ -8,8 +8,22 @@ use Illuminate\Support\Str;
 
 class Validator
 {
-    use Attributes;
+    /**
+     * All the mandatory Snipcart product attributes.
+     *
+     * @var array
+     */
+    protected static $requiredAttributes = ['name', 'id', 'price', 'url'];
 
+    /**
+     * All the optional Snipcart product attributes.
+     *
+     * @var array
+     */
+    protected static $optionalAttributes = [
+        'description', 'image', 'categories', 'metadata', 'weight', 'length', 'height', 'width', 'quantity', 'max-quantity', 'min-quantity', 'stackable', 'quantity-step', 'shippable', 'taxable', 'taxes', 'has-taxes-included', 'file-guid',
+    ];
+    
     /**
      * Validate the attributes.
      *
