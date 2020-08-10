b541a174ba4c5b7f70f1b4aa3bb16074943f2936
Michael Aerni <hello@michaelaerni.ch>
2020-08-10 12:36:11 +0200

    Add original price as placeholder

--- resources/js/components/CurrencyFieldtype.vue
+++ resources/js/components/CurrencyFieldtype.vue
@@ -3,6 +3,7 @@
         <text-input
             type="number"
             :prepend="symbol"
+            :placeholder="originalPrice"
             min="0"
             step="0.01"
             :value="value"
@@ -18,6 +19,11 @@
             return {
                 symbol: this.meta.symbol
             }
+        },
+        computed: {
+            originalPrice() {
+                return this.$store.state.publish.base.values.price;
+            },
         }
     };
 </script>
